5ff9f25ba8305fc8f98ceee8f33acca40cc3b94e
Deniz Deveci <dodeve@gmail.com>
2024-11-25 08:32:57 +0900

    scraped until 1919

--- scrape_tdf_pro.py
+++ scrape_tdf_pro.py
@@ -50,7 +50,7 @@ drop_list = driver.find_elements(By.CLASS_NAME, "pageSelectNav ")
 year_element = drop_list[0].find_elements(By.TAG_NAME, "option")
 year_list = [year.text for year in year_element]
 # use this to choose what year you want to scrape
-# year_list = year_list[21:]
+# year_list = year_list[39:]
 del year_list[0]
 print(year_list)
 
@@ -72,7 +72,7 @@ for year in year_list:
             "https://www.procyclingstats.com/race/tour-de-france/"
             + year
             + "/stage-"
-            + str(i + 1)
+            + stage.split(" ")[1]
         )
         time.sleep(2)
 
@@ -97,79 +97,82 @@ for year in year_list:
                 if k > 0:
                     time_lst.append(t.text)
 
-            for j, _ in enumerate(time_lst):
+            if all(times == "" for times in time_lst):
+                print("entries empty")
+            else:
+                for j, _ in enumerate(time_lst):
 
-                if time_lst[j] != ",," and "-" not in time_lst[j]:
-                    try:
-                        t = datetime.strptime(time_lst[j], "%H:%M:%S")
-                        time_lst[j] = int(
-                            timedelta(
-                                hours=t.hour, minutes=t.minute, seconds=t.second
-                            ).total_seconds()
-                        )
-
-                    except ValueError:
-                        if "," not in time_lst[j]:
-                            t = datetime.strptime(time_lst[j], "%M:%S")
-                            time_lst[j] = int(
-                                timedelta(
-                                    hours=t.hour, minutes=t.minute, seconds=t.second
-                                ).total_seconds()
-                            )
-                        else:
-                            stripped_t = time_lst[j][0 : time_lst[j].index(",")]
-                            t = datetime.strptime(stripped_t, "%M.%S")
+                    if time_lst[j] != ",," and "-" not in time_lst[j]:
+                        try:
+                            t = datetime.strptime(time_lst[j], "%H:%M:%S")
                             time_lst[j] = int(
                                 timedelta(
                                     hours=t.hour, minutes=t.minute, seconds=t.second
                                 ).total_seconds()
                             )
 
-            for j, _ in enumerate(time_lst):
-                if isinstance(time_lst[j], int) and j > 0:
-                    time_lst[j] = time_lst[j] + time_lst[0]
+                        except ValueError:
+                            if "," not in time_lst[j]:
+                                t = datetime.strptime(time_lst[j], "%M:%S")
+                                time_lst[j] = int(
+                                    timedelta(
+                                        hours=t.hour, minutes=t.minute, seconds=t.second
+                                    ).total_seconds()
+                                )
+                            else:
+                                stripped_t = time_lst[j][0 : time_lst[j].index(",")]
+                                t = datetime.strptime(stripped_t, "%M.%S")
+                                time_lst[j] = int(
+                                    timedelta(
+                                        hours=t.hour, minutes=t.minute, seconds=t.second
+                                    ).total_seconds()
+                                )
 
-            for j, _ in enumerate(time_lst):
-                if time_lst[j] == ",,":
-                    time_lst[j] = time_lst[j - 1]
+                for j, _ in enumerate(time_lst):
+                    if isinstance(time_lst[j], int) and j > 0:
+                        time_lst[j] = time_lst[j] + time_lst[0]
 
-            info_lst = [info.text for info in info_element]
-            final_info_lst = []
-            for i in info_lst:
-                if len(i.split("\n")) == 1:
-                    single_el = i.split("\n")
-                    single_el.append("Na")
-                    final_info_lst.append(single_el)
-                else:
-                    final_info_lst.append(i.split("\n"))
+                for j, _ in enumerate(time_lst):
+                    if time_lst[j] == ",,":
+                        time_lst[j] = time_lst[j - 1]
 
-            final_info_lst = np.array(final_info_lst).flatten()
+                info_lst = [info.text for info in info_element]
+                final_info_lst = []
+                for i in info_lst:
+                    if len(i.split("\n")) == 1:
+                        single_el = i.split("\n")
+                        single_el.append("Na")
+                        final_info_lst.append(single_el)
+                    else:
+                        final_info_lst.append(i.split("\n"))
 
-            # construct info table dictionary
-            info_dict = dict(zip(final_info_lst[0::2], final_info_lst[1::2]))
-            info_dict = {
-                k: [v] for k, v in info_dict.items()
-            }  # this is needed as pandas want and index (kinda hack solution)
-            info_df = pd.concat(
-                [info_df, pd.DataFrame.from_dict(info_dict)], ignore_index=True
-            )
-            final_dict = {
-                "year": [int(year)] * len(name_lst),
-                "stage": [stage] * len(name_lst),
-                "name": name_lst,
-                "time": time_lst,
-            }
-            info_dict_ext = {k: v * len(name_lst) for k, v in info_dict.items()}
-            final_dict = final_dict | info_dict_ext
+                final_info_lst = np.array(final_info_lst).flatten()
 
-            df = pd.concat(
-                [
-                    df,
-                    pd.DataFrame(final_dict),
-                ]
-            )
-            df.to_csv("protdf.csv")
-            info_df.to_csv("infodf.csv")
+                # construct info table dictionary
+                info_dict = dict(zip(final_info_lst[0::2], final_info_lst[1::2]))
+                info_dict = {
+                    k: [v] for k, v in info_dict.items()
+                }  # this is needed as pandas want and index (kinda hack solution)
+                info_df = pd.concat(
+                    [info_df, pd.DataFrame.from_dict(info_dict)], ignore_index=True
+                )
+                final_dict = {
+                    "year": [int(year)] * len(name_lst),
+                    "stage": [stage] * len(name_lst),
+                    "name": name_lst,
+                    "time": time_lst,
+                }
+                info_dict_ext = {k: v * len(name_lst) for k, v in info_dict.items()}
+                final_dict = final_dict | info_dict_ext
+
+                df = pd.concat(
+                    [
+                        df,
+                        pd.DataFrame(final_dict),
+                    ]
+                )
+                df.to_csv("protdf.csv")
+                info_df.to_csv("infodf.csv")
         else:
             print("It is a TTT stage")
 
