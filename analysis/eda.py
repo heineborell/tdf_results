938abbec195d257c86f7f2db21abc0dd0d119151
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-01-02 19:21:59 +0900

    added strava_table uploader

--- analysis/eda.py
+++ analysis/eda.py
@@ -1,6 +1,6 @@
 import subprocess
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine, inspect, text
@@ -39,16 +39,16 @@ query_8 = "select trim(cast(x.tdf_date as char)) as tdfdate from( SELECT DISTINC
 
 query_9 = "SELECT p.activity_id, p.athlete_id, cast(p.`date` as char) as date, p.segment, l.stat from segments_data as p left join stats_data as l on l.activity_id=p.activity_id where p.`date` IN( select cast(x.tdf_date as CHAR) as tdfdate FROM( SELECT DISTINCT(w.tdf_date) FROM ( SELECT Date_format( DATE, %s) AS tdf_date FROM tdf_database WHERE year=%s) As w) as x) "
 
-query_10 = "SELECT activity_id, f.`date`, f.dist, f.watt, f.elv,test.vertical_meters,test.distance ,test.lower_bound FROM ( SELECT x.activity_id, `date`, CAST(REGEXP_SUBSTR(x.watts, '[0-9]+') AS UNSIGNED) AS watt, CAST(REGEXP_SUBSTR(x.dist, '[0-9]+') AS UNSIGNED ) AS dist, CAST(REGEXP_REPLACE(REGEXP_SUBSTR(x.elv, '[0-9,]+'), ',', '') AS UNSIGNED) AS elv FROM ( SELECT activity_id, `date`, JSON_EXTRACT(`stat`, '$.wap') AS watts, JSON_EXTRACT(`stat`, '$.elevation') AS elv, JSON_EXTRACT(`stat`, '$.dist') AS dist FROM strava_riders) AS x) AS f LEFT JOIN ( SELECT DISTINCT(stage), distance, vertical_meters,Date_format( `date`, %s ) AS tdf_date, ROUND(distance * 0.2 + distance,3) AS upper_bound, ROUND(distance - distance * 0.2, 3) AS lower_bound FROM tdf_results WHERE YEAR=%s) AS test ON test.tdf_date = f.`date` where   f.dist > test.lower_bound"
+query_10 = """SELECT activity_id, athlete_id, f.`date`, f.elv_strava, test.vertical_meters, f.dist_strava, test.lower_bound, f.watt, stat, segment FROM ( SELECT x.activity_id, x.athlete_id, x.stat, x.segment, `date`, CAST( REGEXP_SUBSTR( x.watts, '[0-9]+') AS UNSIGNED) AS watt, CAST( REGEXP_SUBSTR( x.dist_strava, '[0-9]+')AS UNSIGNED) AS dist_strava, CAST( REGEXP_REPLACE( REGEXP_SUBSTR( x.elv_strava, '[0-9,]+'), ',', '') AS UNSIGNED) AS elv_strava FROM ( SELECT activity_id, athlete_id, stat, segment, `date`, JSON_EXTRACT( `stat`, "$.wap") AS watts, JSON_EXTRACT( `stat`, "$.elevation") AS elv_strava, JSON_EXTRACT( `stat`, "$.dist") AS dist_strava FROM ( SELECT p2.activity_id, p2.athlete_id, CAST( p2.`date` AS CHAR) AS DATE, p2.segment, l2.stat FROM segments_data AS p2 LEFT JOIN stats_data AS l2 ON l2.activity_id = p2.activity_id WHERE p2.`date`IN( SELECT CAST( x2.tdf_date AS CHAR) AS tdfdate FROM ( SELECT DISTINCT( w2.tdf_date) FROM ( SELECT Date_format( DATE, %s ) AS tdf_date FROM giro_results)AS w2)AS x2) )AS stravarider) AS x) AS f LEFT JOIN ( SELECT DISTINCT( stage), distance, vertical_meters, Date_format( `date`, %s )AS tdf_date, ROUND( distance * 0.2 + distance, 3)AS upper_bound, ROUND( distance - distance * 0.2, 3)AS lower_bound FROM giro_results ) AS test ON test.tdf_date = f.`date` WHERE f.dist_strava > test.lower_bound order by DATE"""
 
-df = pd.read_sql_query(query_3, conn)
-df2 = pd.read_sql_query(query_4, conn)
-df3 = pd.read_sql_query(query_5, conn)
-# df4 = pd.read_sql_query(query_6, conn)
-df7 = pd.read_sql_query(query_7, conn, params=("Stage 1 %", 2001))
-df9 = pd.read_sql_query(query_9, conn, params=("%b %d %Y", 2024))
-
-df10 = pd.read_sql_query(query_10, conn, params=("%b %e %Y", 2024))
+# df = pd.read_sql_query(query_3, conn)
+# df2 = pd.read_sql_query(query_4, conn)
+# df3 = pd.read_sql_query(query_5, conn)
+## df4 = pd.read_sql_query(query_6, conn)
+# df7 = pd.read_sql_query(query_7, conn, params=("Stage 1 %", 2001))
+# df9 = pd.read_sql_query(query_9, conn, params=("%b %d %Y", 2024))
+#
+df10 = pd.read_sql_query(query_10, conn, params=("%b %e %Y", "%b %e %Y"))
 # df["customer_name"] = df["customer_first_name"] + " " + df["customer_last_name"]
 # df4["name"] = df4["name"].str.split(r"\s{2,}")[0] + df4["name"].str.split(r"\s{2,}")[1]
 subprocess.run(["vd", "-f", "csv", "-"], input=df10.to_csv(index=False), text=True)
--- data_tools/sql_uploaders/sql_json.py
+++ data_tools/sql_uploaders/sql_json.py
@@ -18,9 +18,8 @@ MYSQL_DATABASE = "grand_tours"
 # Define database connection URL
 DATABASE_URL = "mysql+pymysql://root:Abrakadabra69!@127.0.0.1:3306/grand_tours"
 
-# grand_tour = "giro"
-grand_tour = "tdf"
-year = 2024
+grand_tours = ["tdf", "giro"]
+years = [2023]
 username = getpass.getuser()
 
 segment_range = Path(
@@ -83,77 +82,114 @@ except OperationalError:
     print("Tables do not exist")
 
 # Create the table in the database (if not exists)
-Base.metadata.create_all(engine)
-id_list = []
-for j in range(
-    1, len(sorted(segment_range.glob(f"segment_{year}_{grand_tour}_*"))) + 1
-):
-    if j == 3 and year == 2024:
-        with open(
-            f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/segments/segment_{year}_{grand_tour}_{j}.json",
-            "r",
-        ) as f:
-            json_data = json.loads(f.read())
+for grand_tour in grand_tours:
+    for year in years:
+        Base.metadata.create_all(engine)
+        id_list = []
+        for j in range(
+            1, len(sorted(segment_range.glob(f"segment_{year}_{grand_tour}_*"))) + 1
+        ):
+            if j == 3 and year == 2024 and grand_tour == "tdf":
+                with open(
+                    f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/segments/segment_{year}_{grand_tour}_{j}.json",
+                    "r",
+                ) as f:
+                    json_data = json.loads(f.read())
 
-        for activity in json_data:
-            if activity["activity_id"] not in id_list:
-                row = RawJSONData(
-                    str(activity["activity_id"]),
-                    str(activity["athlete_id"]),
-                    str(activity["date"]).replace("June", "Jun").replace("July", "Jul"),
-                    float(activity["distance"]),
-                    activity["segments"][0],
-                )
-                session.add(row)
-                session.commit()
-                id_list.append(activity["activity_id"])
-                print(f"{j} segment {grand_tour} {year} uploaded")
+                for activity in json_data:
+                    if activity["activity_id"] not in id_list:
+                        row = RawJSONData(
+                            str(activity["activity_id"]),
+                            str(activity["athlete_id"]),
+                            str(activity["date"])
+                            .replace("June", "Jun")
+                            .replace("July", "Jul"),
+                            float(activity["distance"]),
+                            activity["segments"][0],
+                        )
+                        session.add(row)
+                        session.commit()
+                        id_list.append(activity["activity_id"])
+                        print(f"{j} segment {grand_tour} {year} uploaded")
+                    else:
+                        pass
             else:
-                pass
-    else:
 
-        with open(
-            f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/segments/segment_{year}_{grand_tour}_{j}.json",
-            "r",
-        ) as f:
+                with open(
+                    f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/segments/segment_{year}_{grand_tour}_{j}.json",
+                    "r",
+                ) as f:
 
-            json_data = json.loads(f.read())
+                    json_data = json.loads(f.read())
 
-        for activity in json_data[0]["activities"]:
-            if activity["activity_id"] not in id_list:
-                row = RawJSONData(
-                    str(activity["activity_id"]),
-                    str(activity["athlete_id"]),
-                    str(activity["date"]).replace("June", "Jun").replace("July", "Jul"),
-                    float(activity["distance"]),
-                    activity["segments"][0],
-                )
-                session.add(row)
-                session.commit()
-                id_list.append(activity["activity_id"])
-                print(f"{j} segment {grand_tour} {year} uploaded")
-            else:
-                pass
-id_list = []
-for j in range(1, len(sorted(stat_range.glob(f"stat_{year}_{grand_tour}_*"))) + 1):
-    with open(
-        f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/stats/stat_{year}_{grand_tour}_{j}.json",
-        "r",
-    ) as f:
-        json_data = json.loads(f.read())
+                for activity in json_data["activities"]:
+                    if activity["activity_id"] not in id_list:
+                        row = RawJSONData(
+                            str(activity["activity_id"]),
+                            str(activity["athlete_id"]),
+                            str(activity["date"])
+                            .replace("June", "Jun")
+                            .replace("July", "Jul"),
+                            float(activity["distance"]),
+                            activity["segments"][0],
+                        )
+                        session.add(row)
+                        session.commit()
+                        id_list.append(activity["activity_id"])
+                        print(f"{j} segment {grand_tour} {year} uploaded")
+                    else:
+                        pass
+        id_list = []
+        for j in range(
+            1, len(sorted(stat_range.glob(f"stat_{year}_{grand_tour}_*"))) + 1
+        ):
+            with open(
+                f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/stats/stat_{year}_{grand_tour}_{j}.json",
+                "r",
+            ) as f:
+                json_data = json.loads(f.read())
 
-    for activity in json_data[0]["stats"]:
-        if activity["activity_id"] not in id_list:
-            row = RawJSONData_2(
-                str(activity["activity_id"]),
-                str(activity["athlete_id"]),
-                dict(list(activity.items())[2:]),
-            )
-            session.add(row)
-            session.commit()
-            id_list.append(activity["activity_id"])
-            print(f"{j} stats {grand_tour} {year} uploaded")
-        else:
-            pass
-
+            for activity in json_data["stats"]:
+                if activity["activity_id"] not in id_list:
+                    row = RawJSONData_2(
+                        str(activity["activity_id"]),
+                        str(activity["athlete_id"]),
+                        dict(list(activity.items())[2:]),
+                    )
+                    session.add(row)
+                    session.commit()
+                    id_list.append(activity["activity_id"])
+                    print(f"{j} stats {grand_tour} {year} uploaded")
+                else:
+                    pass
 print("JSON data uploaded successfully.")
+
+query_giro = """(SELECT activity_id, athlete_id, f.`date`, f.elv_strava, test.vertical_meters, f.dist_strava, test.lower_bound, f.watt, stat, segment FROM ( SELECT x.activity_id, x.athlete_id, x.stat, x.segment, `date`, CAST( REGEXP_SUBSTR( x.watts, '[0-9]+') AS UNSIGNED) AS watt, CAST( REGEXP_SUBSTR( x.dist_strava, '[0-9]+')AS UNSIGNED) AS dist_strava, CAST( REGEXP_REPLACE( REGEXP_SUBSTR( x.elv_strava, '[0-9,]+'), ',', '') AS UNSIGNED) AS elv_strava FROM ( SELECT activity_id, athlete_id, stat, segment, `date`, JSON_EXTRACT( `stat`, "$.wap") AS watts, JSON_EXTRACT( `stat`, "$.elevation") AS elv_strava, JSON_EXTRACT( `stat`, "$.dist") AS dist_strava FROM ( SELECT p2.activity_id, p2.athlete_id, CAST( p2.`date` AS CHAR) AS DATE, p2.segment, l2.stat FROM segments_data AS p2 LEFT JOIN stats_data AS l2 ON l2.activity_id = p2.activity_id WHERE p2.`date`IN( SELECT CAST( x2.tdf_date AS CHAR) AS tdfdate FROM ( SELECT DISTINCT( w2.tdf_date) FROM ( SELECT Date_format( DATE, :s ) AS tdf_date FROM giro_results)AS w2)AS x2) )AS stravarider) AS x) AS f LEFT JOIN ( SELECT DISTINCT( stage), distance, vertical_meters, Date_format( `date`, :s )AS tdf_date, ROUND( distance * 0.2 + distance, 3)AS upper_bound, ROUND( distance - distance * 0.2, 3)AS lower_bound FROM giro_results ) AS test ON test.tdf_date = f.`date` WHERE f.dist_strava > test.lower_bound order by DATE) as giro_table """
+
+query_tdf = """(SELECT activity_id, athlete_id, f.`date`, f.elv_strava, test.vertical_meters, f.dist_strava, test.lower_bound, f.watt, stat, segment FROM ( SELECT x.activity_id, x.athlete_id, x.stat, x.segment, `date`, CAST( REGEXP_SUBSTR( x.watts, '[0-9]+') AS UNSIGNED) AS watt, CAST( REGEXP_SUBSTR( x.dist_strava, '[0-9]+')AS UNSIGNED) AS dist_strava, CAST( REGEXP_REPLACE( REGEXP_SUBSTR( x.elv_strava, '[0-9,]+'), ',', '') AS UNSIGNED) AS elv_strava FROM ( SELECT activity_id, athlete_id, stat, segment, `date`, JSON_EXTRACT( `stat`, "$.wap") AS watts, JSON_EXTRACT( `stat`, "$.elevation") AS elv_strava, JSON_EXTRACT( `stat`, "$.dist") AS dist_strava FROM ( SELECT p2.activity_id, p2.athlete_id, CAST( p2.`date` AS CHAR) AS DATE, p2.segment, l2.stat FROM segments_data AS p2 LEFT JOIN stats_data AS l2 ON l2.activity_id = p2.activity_id WHERE p2.`date`IN( SELECT CAST( x2.tdf_date AS CHAR) AS tdfdate FROM ( SELECT DISTINCT( w2.tdf_date) FROM ( SELECT Date_format( DATE, :s ) AS tdf_date FROM tdf_results)AS w2)AS x2) )AS stravarider) AS x) AS f LEFT JOIN ( SELECT DISTINCT( stage), distance, vertical_meters, Date_format( `date`, :s )AS tdf_date, ROUND( distance * 0.2 + distance, 3)AS upper_bound, ROUND( distance - distance * 0.2, 3)AS lower_bound FROM tdf_results) AS test ON test.tdf_date = f.`date` WHERE f.dist_strava > test.lower_bound order by DATE) as tdf_table """
+
+# Create database engine and session
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+try:
+    with engine.connect() as connection:
+        connection.execute(text("DROP TABLE strava_table"))
+
+except OperationalError:
+    print("Tables do not exist")
+
+
+query = (
+    "CREATE TABLE strava_table AS SELECT * FROM "
+    + query_tdf
+    + " UNION SELECT * FROM "
+    + query_giro
+)
+
+with engine.connect() as connection:
+    stmt = text(query)
+    stmt = stmt.bindparams(s="%b %e %Y")
+    res = session.execute(stmt)
+    print("strava_table formed.")
