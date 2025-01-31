9dfac79a52e76d3983702726531825d77a11f7e8
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-01-31 22:36:15 +0900

    finished the segments part of jsoniser

--- data_tools/data_cleaning/segment_jsoniser.py
+++ data_tools/data_cleaning/segment_jsoniser.py
@@ -1,15 +1,14 @@
 import getpass
 import gzip
+import json
 import pickle
 import re
 
-import numpy as np
-import pandas as pd
 from bs4 import BeautifulSoup
 
 username = getpass.getuser()
 with gzip.open(
-    f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/tdf_pickles/segment_6182498304_2024_tdf.pkl.gz",
+    f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/strava/tdf_pickles/segment_6175322112_2018_tdf.pkl.gz",
     "rb",
 ) as fp:  # Pickling
     data = pickle.load(fp)
@@ -74,8 +73,11 @@ for i, no in enumerate(activity_dict_list):
 #    ".dense.hidden-segments.hoverable.marginless"
 # )
 # print(data[2][0][4])
-for key, element in enumerate(data[1:20]):
-    segment_table = BeautifulSoup(element[0][4][0], "html.parser")
+for key, element in enumerate(data):
+    try:
+        segment_table = BeautifulSoup(element[0][4][0], "html.parser")
+    except IndexError:
+        segment_table = BeautifulSoup("None", "html.parser")
     # Initialize empty dictionary
     segment_dict = {}
 
@@ -164,23 +166,65 @@ for key, element in enumerate(data[1:20]):
         else:
             segment_dict = {}
 
+        # Extract Climb Category
+        climb_td = segment.find("td", class_="climb-cat-col")
+        climb_category = climb_td.text.strip() if climb_td else "N/A"
+
+        # Extract Time
+        time_td = segment.find("td", class_="time-col")
+        segment_time.append(time_td.get_text(strip=True) if time_td else "N/A")
+
+        # Extract Speed
+        speed_td = segment.find_all("td")[6]  # Speed is in the 5th <td> (index 4)
+        segment_speed.append(speed_td.get_text(strip=True) if speed_td else "N/A")
+
+        # Extract Watts
+        watts_td = segment.find_all("td")[7]  # Watts is in the 6th <td> (index 5)
+        watt.append(watts_td.get_text(strip=True) if watts_td else "N/A")
+
+        # Extract VAM (optional, may not exist)
+        vam_td = segment.find_all("td")[8] if len(segment.find_all("td")) > 8 else None
+        VAM.append(vam_td.get_text(strip=True) if vam_td else "N/A")
+
+        # Extract Heart Rate
+        heart_rate_td = (
+            segment.find_all("td")[9] if len(segment.find_all("td")) > 9 else None
+        )
+        heart_rate.append(
+            heart_rate_td.get_text(strip=True) if heart_rate_td else "N/A"
+        )
+
         ## Assign extracted data to the dictionary
         segment_dict = {
-            "segment_no": segment_no,
+            # "segment_no": segment_no,
+            # 'climb_category': climb_category,
             "segment_name": segment_name,
-            #    "segment_time": segment_time,
-            #    "segment_speed": segment_speed,
-            #    "watt": watt,
-            #    "heart_rate": heart_rate,
+            "segment_time": segment_time,
+            "segment_speed": segment_speed,
+            "watt": watt,
+            "heart_rate": heart_rate,
             "segment_distance": segment_distance,
             "segment_vert": segment_vert,
             "segment_grade": segment_grade,
-            #    "VAM": VAM,
+            "VAM": VAM,
         }
     activity_dict_list[key].update({"segments": segment_dict})
 
-print(activity_dict_list)
+    json_string = json.dumps(activity_dict_list)
+    with open(
+        f"segment.json",
+        "w",
+    ) as f:
+        f.write(json_string)
 
+        # with open(
+        #    f"inner_2.txt",
+        #    "w",
+        # ) as f:
+        #    try:
+        #        f.write(element[0][4][0])
+        #    except IndexError:
+        #        pass
 
 # if len(fields) > 5:
 #    segment_time.append(fields[5].get_text())
