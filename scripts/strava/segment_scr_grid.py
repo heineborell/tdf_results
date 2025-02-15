9962b6dc87bb5a8eeb73ebccdc18075631d8eb2d
Deniz Deveci <dodeve@gmail.com>
2025-02-15 17:35:23 +0900

    cleaning of repo

--- scripts/strava/segment_scr_grid.py
+++
@@ -1,5 +0,0 @@
-from grand_tours import segment_scraper
-
-segmentscr = segment_scraper.SegmentScrape("tdf", 2024)
-
-segmentscr.scrape()
--- scripts/strava/test.py
+++
@@ -1,8 +0,0 @@
-import getpass
-
-import grand_tours
-from grand_tours import jsonisers
-
-if __name__ == "__main__":
-    username = getpass.getuser()
-    print(jsonisers.stat_jsoniser(username, "tdf", 2018))
